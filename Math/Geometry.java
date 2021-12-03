f9264e099c9883901262f01b2fd85af586180c5b
Henri <henriboraschiventurini@gmail.com>
2021-12-03 11:46:37 -0300

    Geometry Class done

--- Math/Geometry.java
+++ Math/Geometry.java
@@ -14,7 +14,7 @@ public class Geometry{
 	//PERIMETER
 	
     public static float perimeter(float p[]){
-
+		// needed a new validator
         for(int i = 0; i < p.length; i++){
             result+=p[i];
         }
@@ -23,7 +23,7 @@ public class Geometry{
     }
 	
     public static float circlePerimeter(float r){
-
+		validator(r);
         result = 2 * PI * r;
         return result;
     }
@@ -31,31 +31,31 @@ public class Geometry{
 	// 2D AREA
 	
 	public static float rectanguleArea(float h, float b){
-		
+		validator(b, h);
 		result = h * b;
 		return result;
 	}
 	
 	public static float circleArea(float r){
-		
+		validator(r);
 		result = PI * (r * r);
 		return result;
 	}
 	
 	public static float equilateralTriangleArea(float s){
-		
+		validator(s);
 		result = (float) (((s * s)* Math.sqrt(3))/4);
 		return result;
 	}
 	
 	public static float genericTriangle(float h, float b){
-		
+		validator(b, h);
 		result = (b * h)/2;
 		return result;
 	}
 	
 	public static float trapezoidArea(float bigBase, float b, float h){
-		
+		validator(bigBase, h, b);
 		result = ((bigBase * b)*h)/ 2;
 		return result;
 	}
@@ -63,31 +63,31 @@ public class Geometry{
 	// 3D AREA
 	
 	public static float sphereArea(float r){
-		
+		validator(r);
 		result = 4 * PI * (r * r);
 		return result;
 	}
 	
 	public static float prismArea(float baseArea, float sideArea, int totalNumberSides){
-		
+		validator(baseArea, sideArea, totalNumberSides);
 		result = (2 * baseArea) + (totalNumberSides * sideArea);
 		return result;
 	}
 	
 	public static float cubeArea(float s){
-		
+		validator(s);
 		result = (s * s) * 6;
 		return result;
 	}
 	
 	public static float cilinderArea(float r, float h){
-		
+		validator(r, h);
 		result = 2 * PI * r *(r + h);
 		return result;
 	}
 	
 	public static float coneArea(float r, float h){
-		
+		validator(r, h);
 		result = (PI * (r * r)) + (PI * r * pitagoras(h,r));
 		return result;
 	}
@@ -95,7 +95,7 @@ public class Geometry{
 	// FORMULA
 	
 	public static float pitagoras(float a, float b){
-		
+		validator(a, b);
 		result = (a * a) +  (b * b);
 		return (float) Math.sqrt(result);
 	}
@@ -103,31 +103,32 @@ public class Geometry{
 	// VOLUME 
 	
 	public static float sphereVolume(float r){
-		
+		validator(r);
 		result = (4 / 3) * PI * (r * r * r);
 		return result;
 	}
 	
 	public static float prismVolume(float baseArea, float h){
-		
+		validator(baseArea, h);
 		result = baseArea * h;
 		return result;
 	}
 	
 	public static float cubeVolume(float s){
-		
+		validator(s);
 		result = s * s * s;
 		return result;
 	}
 	
 	public static float cilinderVolume(float r, float h){
-		
+		validator(r, h);
 		result = PI * (r * r) * h;
 		return result;
 	}
 	
 	public static float coneVolume(float r, float h){
 		
+		validator(r, h);
 		result = (1 / 3) * (PI * (r * r)) * h;
 		return result;
 	}
